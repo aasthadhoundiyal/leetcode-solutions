b81362e0e02e763e84f3ce54df4452ffd4953520
aasthadhoundiyal <dhoundiyalaastha@gmail.com>
2026-08-25 08:17:58 +0530

    Time: 1 ms (99.65%), Space: 47.2 MB (89.9%) - LeetHub

---
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int[] ans = new int[2];
+        int n = grid.length;
+        int[] freq = new int[n*n + 1];
+        for(int i=0; i<grid.length ; i++){
+            for(int j=0 ; j<grid[0].length ; j++){
+                freq[grid[i][j]]++;
+            }
+        }
+        for(int i=1; i<=n*n ; i++){
+            if (freq[i]==2){
+                ans[0] = i;
+            }
+            else if (freq[i]==0){
+                ans[1] = i;
+            }
+            
+        }
+        return ans;
+        
+        
+    }
+}
