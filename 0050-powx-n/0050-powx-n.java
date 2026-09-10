2974055e1f60416fffdbbe123ef89dfba7755932
aasthadhoundiyal <dhoundiyalaastha@gmail.com>
2026-09-10 08:52:30 +0530

    Time: 0 ms (100%), Space: 48 MB (55.99%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,38 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
-        
-    }
+        double ans = 1;
+        long binForm = n;
+
+        if(n==0 || x==1){
+            return 1;
+        }
+        if (x==-1 && (n%2==0)){
+            return 1;
+        }
+        if (x==-1 && (n%2!=0)){
+            return -1;
+        }
+
+        if (n < 0){
+            x=1/x;
+            binForm=-binForm;
+
+        }
+
+        while (binForm>0){
+            if (binForm%2==1){
+                ans*=x;
+            }
+            x*=x;
+            binForm/=2;
+        }
+
+        return ans;
+
+    }   
+
+       
 }
+    
+
+    
